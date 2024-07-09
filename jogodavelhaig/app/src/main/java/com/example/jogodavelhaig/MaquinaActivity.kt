6b6a5e3f6e9f7e64c79a8a2772f719122390423b
igorribeir <166820593+igorribeir@users.noreply.github.com>
2024-07-09 13:50:13 -0300

    MaquinaActivity.kt

--- jogodavelhaig/app/src/main/java/com/example/jogodavelhaig/MaquinaActivity.kt
+++ jogodavelhaig/app/src/main/java/com/example/jogodavelhaig/MaquinaActivity.kt
@@ -9,83 +9,79 @@ import com.example.jogodavelhaig.databinding.ActivityMainBinding
 
 import kotlin.random.Random
 
-class MaquinaActivity : AppCompatActivity() {
-    // Variável de ligação para acesso às views
-    private lateinit var binding: ActivityMainBinding
+class MaquinaActivity : AppCompatActivity()  {
+    private lateinit var binding:ActivityMainBinding
 
-    // Vetor bidimensional que representará o tabuleiro de jogo
+    //Vetor bidimensional que representará o tabuleiro de jogo
     val tabuleiro = arrayOf(
         arrayOf("A", "B", "C"),
         arrayOf("D", "E", "F"),
         arrayOf("G", "H", "I")
     )
 
-    // Método onCreate que é chamado quando a Activity é criada
-    // Entrada: savedInstanceState - o estado salvo da Activity
-    // Saída: Nenhuma
+    //Qual o Jogador está jogando
+    var jogadorAtual = "X"
+
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Infla o layout usando o binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge() // Método para ativar bordas
-        setContentView(binding.root) // Define o layout da Activity como a raiz do binding
+        enableEdgeToEdge()
+        setContentView(binding.root)
     }
 
-    // Função que será chamada quando um botão for clicado
-    // Entrada: view - a view que foi clicada
-    // Saída: Nenhuma
-    fun buttonClick(view: View){
-        // Converte a view recebida para um botão
+    //Função associada com todos os botões @param view é o botão clicado
+    fun buttonClick(view: View) {
+        //O botão clicado é associado com uma constante
         val buttonSelecionado = view as Button
 
-        // Define o texto do botão clicado como "X"
-        buttonSelecionado.setBackgroundResource(R.drawable.goat)
-
-        // Desativa o botão para que não possa ser clicado novamente
-        buttonSelecionado.isEnabled = false
-
-        // Atualiza o tabuleiro com "X" na posição correspondente ao botão clicado
+        //De acordo com o botão clicado, a posição da matriz receberá o Jogador
         when(buttonSelecionado.id){
-            binding.buttonZero.id -> tabuleiro[0][0] = "X"
-            binding.buttonUm.id -> tabuleiro[0][1] = "X"
-            binding.buttonDois.id -> tabuleiro[0][2] = "X"
-            binding.buttonTres.id -> tabuleiro[1][0] = "X"
-            binding.buttonQuatro.id -> tabuleiro[1][1] = "X"
-            binding.buttonCinco.id -> tabuleiro[1][2] = "X"
-            binding.buttonSeis.id -> tabuleiro[2][0] = "X"
-            binding.buttonSete.id -> tabuleiro[2][1] = "X"
-            binding.buttonOito.id -> tabuleiro[2][2] = "X"
+            binding.buttonZero.id -> tabuleiro[0][0] = jogadorAtual
+            binding.buttonUm.id -> tabuleiro[0][1] = jogadorAtual
+            binding.buttonDois.id -> tabuleiro[0][2] = jogadorAtual
+            binding.buttonTres.id -> tabuleiro[1][0] = jogadorAtual
+            binding.buttonQuatro.id -> tabuleiro[1][1] = jogadorAtual
+            binding.buttonCinco.id -> tabuleiro[1][2] = jogadorAtual
+            binding.buttonSeis.id -> tabuleiro[2][0] = jogadorAtual
+            binding.buttonSete.id -> tabuleiro[2][1] = jogadorAtual
+            binding.buttonOito.id -> tabuleiro[2][2] = jogadorAtual
         }
 
-        // Gera posições aleatórias para o próximo movimento do computador
+        buttonSelecionado.setBackgroundResource(R.drawable.goat)
+        buttonSelecionado.isEnabled=false
+
+        //Recebe o jogador vencedor através da função verificaTabuleiro. @param tabuleito
+        var vencedor = verificaVencedor(tabuleiro)
+
+        if(!vencedor.isNullOrBlank()) {
+            Toast.makeText(this, "Vencedor: " + vencedor, Toast.LENGTH_LONG).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
         var rX = Random.nextInt(0, 3)
         var rY = Random.nextInt(0, 3)
-
-        // Tenta encontrar uma posição vazia no tabuleiro
-        var i = 0
+var i = 0
         while (i < 9) {
             rX = Random.nextInt(0, 3)
             rY = Random.nextInt(0, 3)
 
             if (tabuleiro[rX][rY] != "X" && tabuleiro[rX][rY] != "O") {
-                break // Sai do loop se encontrar uma posição vazia
+                break
             }
 
             i++
         }
 
-        // Marca a posição encontrada com "O"
-        tabuleiro[rX][rY] = "O"
+        tabuleiro[rX][rY]="O"
 
-        // Converte a posição bidimensional para uma posição linear
-        val posicao = rX * 3 + rY
+        val posicao = rX*3 + rY
 
-        // Atualiza o texto e estado do botão correspondente no layout
         when(posicao){
             0 -> {
                 binding.buttonZero.setBackgroundResource(R.drawable.penaldo)
                 binding.buttonZero.isEnabled = false
-
             }
             1 -> {
                 binding.buttonUm.setBackgroundResource(R.drawable.penaldo)
@@ -120,5 +116,51 @@ class MaquinaActivity : AppCompatActivity() {
                 binding.buttonOito.isEnabled = false
             }
         }
+
+        vencedor = verificaVencedor(tabuleiro)
+
+        if(!vencedor.isNullOrBlank()) {
+            Toast.makeText(this, "Vencedor: " + vencedor, Toast.LENGTH_LONG).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    fun verificaVencedor(tabuleiro: Array<Array<String>>): String? {
+
+        // Verifica linhas e colunas
+        for (i in 0 until 3) {
+            //Verifica se há três itens iguais na linha
+            if (tabuleiro[i][0] == tabuleiro[i][1] && tabuleiro[i][1] == tabuleiro[i][2]) {
+                return tabuleiro[i][0]
+            }
+            //Verifica se há três itens iguais na coluna
+            if (tabuleiro[0][i] == tabuleiro[1][i] && tabuleiro[1][i] == tabuleiro[2][i]) {
+                return tabuleiro[0][i]
+            }
+        }
+        // Verifica diagonais
+        if (tabuleiro[0][0] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][2]) {
+            return tabuleiro[0][0]
+        }
+        if (tabuleiro[0][2] == tabuleiro[1][1] && tabuleiro[1][1] == tabuleiro[2][0]) {
+            return tabuleiro[0][2]
+        }
+        //Verifica a quantidade de jogadores 
+        var empate = 0
+        for (linha in tabuleiro) {
+            for (valor in linha) {
+                if(valor.equals("X")||valor.equals("O")){
+                    empate++
+                }
+            }
+        }
+        //Se existem 9 jogadas e não há três letras iguais, houve um empate
+        if(empate == 9){
+            return "Empate"
+        }
+        // Nenhum vencedor
+        return null
     }
 }
